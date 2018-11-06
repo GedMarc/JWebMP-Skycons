73eeb9cdecb8b2cc7aa647f8d716bd4dc72d591c
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:35 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/skycons/implementations/SkyconsExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.skycons.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SkyconsExclusionsModule
+		implements IGuiceScanModuleExclusions<SkyconsExclusionsModule>,
+				           IGuiceScanJarExclusions<SkyconsExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-skycons-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.skycons");
+		return strings;
+	}
+}
