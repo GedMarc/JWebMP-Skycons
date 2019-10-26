986945d94016cefd2536cfc76bb53a4805e339a3
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:51 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -5,12 +5,12 @@ module com.jwebmp.plugins.skycons {
 	requires com.jwebmp.core;
 	requires java.validation;
 	requires com.jwebmp.interception;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.skycons.configurator.SkyconPageConfigurator;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.skycons.implementations.SkyconsExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.skycons.implementations.SkyconsExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.skycons.implementations.SkyconsExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.skycons.implementations.SkyconsExclusionsModule;
 
 	opens com.jwebmp.plugins.skycons.configurator;
 	opens com.jwebmp.plugins.skycons to com.jwebmp.core;
